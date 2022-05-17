4d7e16c07c171d278229ec7cac49bb56e3d05be7
Sakil <mdsakiluzzaman@gmail.com>
2022-05-17 18:34:47 +0600

    use state basics

---
+++ src/setState/Counter.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment() {
+    // this.setState(
+    //   {
+    //     count: this.state.count + 1,
+    //   },
+    //   () => {
+    //     console.log("callback value", this.state.count); //callback function
+    //   }
+    // );
+    this.setState((prevState,props) =>({
+        count:prevState.count+1
+    }))
+    console.log(this.state.count);
+  }
+  incrementFive() {
+    this.increment();
+    this.increment();
+    this.increment();
+    this.increment();
+    this.increment();
+  }
+  render() {
+    return (
+      <div>
+        <div>Count - {this.state.count}</div>
+        <button onClick={() => this.incrementFive()}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
+
+
+//never modify the state directly 
+//for updating state based of the previous satate value, pass in a f unciton as an argumnet insted of regular object.
+
